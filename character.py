5df4ef3faf5d14a68d894fc1267143095b66ee17
Jonathan Jared Nuñez Juarez <61926671+jonnyyl@users.noreply.github.com>
2022-12-03 03:41:27 +0000

    Se Agrego propiedades y metodos

--- character.py
+++ character.py
@@ -1,2 +1,16 @@
+from catalog.pokemons import POKEMONS
+
 class Character():
-    pass
+    name = " "
+    health = 0
+    speed = 0 
+    defense = 0
+    moves = " "
+
+    def call_name(self):
+        self.name = input().lower()
+        return self.name 
+
+    def call_health(self):
+        self.health = POKEMONS[self.name]["hp"]
+        return self.health
--- main.py
+++ main.py
@@ -1,1 +1,10 @@
 #Primer Commit 
+from character import Character
+
+character = Character()
+
+
+character.call_name()
+print(character.name)
+character.call_health()
+print(character.health)
