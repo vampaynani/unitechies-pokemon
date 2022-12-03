76c9e4dc8d750e5cd50c3efecc37865730d593b8
Joel-OM29 <111807577+Joel-OM29@users.noreply.github.com>
2022-12-03 03:46:40 +0000

    Cree los metodos de la clase battle

--- battle.py
+++ battle.py
@@ -1,2 +1,16 @@
+#from player import Player
+#from bot import Bot
+
 class Battle():
-    pass
+    def introduction(self):
+        print ("Bienvenidos a una pelea de pokemones entre: ")
+
+    def run(self):
+        pass
+  
+
+    def show_winer(self):
+        pass
+
+    def who_goes_first(self):
+       pass
