4cb3359a5a679695c6f79afa41ac552f14af89a3
TAQUITOR <diegoxpc55@hotmail.com>
2022-12-04 22:19:38 -0600

    Modifique el diccionario para usarlo como una lista

--- catalog/pokemons.py
+++ catalog/pokemons.py
@@ -1,41 +1,46 @@
-POKEMONS = {
-    "bulbasaur": {
+POKEMONS = [
+    {
+        "name": "bulbasaur", 
         "hp": 45,
         "speed": 45,
         "defense": 49,
-        "moves": ["tackle", "vine whip"],
+        "moves": ["tackle", "vine whip"]
     },
-    "charmander": {
+    {   "name" : "charmander",
         "hp": 39,
         "speed": 65,
         "defense": 43,
-        "moves": ["tackle", "fire punch"],
+        "moves": ["tackle", "fire punch"]
     },
-    "squirtle": {
+    {
+        "name": "squirtle",
         "hp": 44,
         "speed": 43,
         "defense": 65,
-        "moves": ["tackle", "water gun"],
+        "moves": ["tackle", "water gun"]
     },
-    "pikachu": {
+    {
+        "name" : "pikachu",
         "hp": 35,
         "speed": 90,
         "defense": 40,
-        "moves": ["tackle", "thunder shock"],
+        "moves": ["tackle", "thunder shock"]
     },
-    "snorlax": {
+    {
+        "name":"snorlax",
         "hp": 160,
         "speed": 30,
         "defense": 65,
         "moves": ["tackle", "mega punch"],
     },
-    "pidgeotto": {
+    {
+        "name":"pidgeotto",
         "hp": 63,
         "speed": 71,
         "defense": 55,
         "moves": ["tackle", "wing attack"],
     }
-}
+]
 
 pokemones = ["bulbasaur", "charmander", "squirtle", "pikachu", "snorlax", "pidgeotto"]
 poderes = ["tackle", "vine whip","fire punch", "water gun","thunder shock","mega punch","wing attack"]
